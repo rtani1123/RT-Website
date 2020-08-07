a0050b5c10ef3bb9604c5ac58b992802e7d32aff
Ryan Tani <ryan.tani1123@gmail.com>
2020-08-07 18:48:00 -0400

    contact form update

--- php/contactform.php
+++ php/contactform.php
@@ -1,17 +1,20 @@
 <?php
 
-if(isset($_POST['submit'])) {
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    $mailTo = "ryanytani@gmail.com"
-    $headers = "From: ".$mailFrom;
-    $message = "You have received an email from ".$name.".\n\n".$message;
+    $mailTo = "ryanytani@gmail.com";
+    $headers = 'From: ' . $mailFrom . "\r\n" .
+        'Reply-To: ' . $mailFrom . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
     mail($mailTo, $subject, $message, $headers);
-    header("Location: contact.html?mailsend");
+?>
+    Thank you for writing! I will be in touch with you very soon.
+<?php
 }
 
 ?>
