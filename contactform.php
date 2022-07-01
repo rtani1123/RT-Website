fe82a44fc5ee3b50621c24d0563c48d8abaaa86c
Ryan Tani <ryanytani@gmail.com>
2022-07-01 12:13:31 -0600

    fixed contact form

--- contactform.php
+++ contactform.php
@@ -1,21 +1,58 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['sender-name'];
-    $subject = 'Email from: ' + $name;
-    $mailFrom = $_POST['sender-email'];
-    $message = $_POST['sender-message'];
+$nameErr = $emailErr = $messageErr = "";
+$name = $email = $message = $headers = "";
+$allValid = true;
 
-    $mailTo = "ryanytani@gmail.com";
-    $headers = 'From: ' . $mailFrom . "\r\n" .
-        'Reply-To: ' . $mailFrom . "\r\n" .
-        'X-Mailer: PHP/' . phpversion();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+        $allValid = false;
+    } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+            $allValid = false;
+        }
+    }
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+        $allValid = false;
+    } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+            $allValid = false;
+        }
+        $headers = "From: ryan@ryanytani.com";
+    }
 
-    mail($mailTo, $subject, $message, $headers);
-    header("Location: index.html?mailsend");
+    if (empty($_POST["message"])) {
+        $message = "";
+        $allValid = false;
+    } else {
+        $message = test_input($_POST["message"]);
+    }
+    if ($allValid) :
+        mail("ryanytani@gmail.com", "Message from " . $name . " at " . $email, $message, $headers);
 ?>
-    Thank you for writing! I will be in touch with you very soon.
-<?php
+        <h2>Thanks for reaching out!</h2>
+        <h3>Confirmation of message sent:</h3>
+        <p>Name: <?php echo $name; ?></p>
+        <p>Email: <?php echo $email; ?></p>
+        <p>Message: <?php echo $message; ?></p>
+    <?php else : ?>
+        <h2> ERROR </h2>
+<?php endif;
 }
 
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
